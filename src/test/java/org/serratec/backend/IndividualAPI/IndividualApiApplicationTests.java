62aa43c14ccaed8fac433cdca9658329373efa7a
iza.pdelucca <bellapdelucca@gmail.com>
2024-05-15 11:15:30 -0300

    first commit

---
+++ src/test/java/org/serratec/backend/IndividualAPI/IndividualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.IndividualAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
